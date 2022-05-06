8ba8b645bc5bc2c66ed2570f77283aab65d78f15
IAlekseiI <94071321+IAlekseiI@users.noreply.github.com>
2022-05-06 19:58:39 +0300

    Add files via upload

---
+++ decrypt_binary.py
@@ -0,0 +1,42 @@
+
+in_file = open('c:/draft/out.txt','r', encoding= 'utf-16')
+out_file = open('c:/draft/decr.txt','w+', encoding= 'utf-16')
+
+key = 'mouse'
+len_key = len(key)
+
+with in_file as my_file:
+    # итерация по строкам
+    for line in my_file:
+        len_str = (len(line)) #  длина строки
+        # line   строка
+        print('\n')
+        n_string=[]
+        
+        for w in range(0,len(line), 9):
+            k = len(line)//len_key+1
+            n_key= key*k
+            w_string=line[w:w+8]
+            
+            if w_string != '\n' and len(w_string)>=8:
+                d_string = int(w_string, base = 2)
+            #char=chr(d_string[w]^ord(n_key[w]))
+               
+            n_string.append(int(d_string))
+        print(n_string)
+        for i in range(0, len(n_string)):
+            chunk = n_string[i]^ord(n_key[i])
+            if chunk == 103 or chunk == 127:
+                n = ' '    
+            n=chr(chunk)
+            if n != '103':
+                out_file.write(n)
+
+
+
+
+
+out_file.close()
+in_file.close()
+
+
